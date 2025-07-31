76e95661f5d77992584d09c5f9e0d9e16f32b83f
M-SaaD-H <mohd.saadhaider@gmail.com>
2025-07-31 10:29:27 +0530

    Extension is ready

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,17 +1,56 @@
-const video = document.querySelector("video");
+// Function to set up video event listeners
+function setupVideoListeners(video) {
+  if (video && !video.hasAttribute("data-autotone-initialized")) {
+    video.setAttribute("data-autotone-initialized", "true");
+    
+    video.addEventListener("play", () => {
+      console.log("video play");
+      chrome.runtime.sendMessage({ event: "youtube_play" });
+    });
+    
+    video.addEventListener("pause", () => {
+      console.log("video pause");
+      chrome.runtime.sendMessage({ event: "youtube_pause" });
+    });
 
-if (video) {
-  console.log("got the video:", video)
-  video.addEventListener("play", () => {
-    console.log("video play")
-    chrome.runtime.sendMessage({ event: "youtube_play" });
-  });
-  video.addEventListener('pause', () => {
-    console.log("video pause")
-    chrome.runtime.sendMessage({ event: "youtube_pause" });
-  });
+    video.pause();
+  }
 }
 
-// chrome.runtime.sendMessage({test: "hello from content script"}, response => {
-//   console.log("Background responded:", response);
-// });
+// Function to find and set up all video elements
+function findAndSetupVideos() {
+  const videos = document.querySelectorAll("video");
+  videos.forEach(video => setupVideoListeners(video));
+}
+
+// Set up initial videos
+findAndSetupVideos();
+
+// Use MutationObserver to detect when new video elements are added
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    // Check for added nodes
+    mutation.addedNodes.forEach((node) => {
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        // Check if the added node is a video
+        if (node.tagName === "VIDEO") {
+          setupVideoListeners(node);
+        }
+        // Check if the added node contains videos
+        const videos = node.querySelectorAll ? node.querySelectorAll("video") : [];
+        videos.forEach(video => setupVideoListeners(video));
+      }
+    });
+  });
+});
+
+// Start observing
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
+
+// Also check periodically for videos that might have been missed
+setInterval(() => {
+  findAndSetupVideos();
+}, 2000);
--- src/worker.js
+++ src/worker.js
@@ -1,12 +1,27 @@
-console.log("AutoDucker service worker loaded");
+console.log("AutoTone service worker loaded");
+
+// Reload the youtube tab to execute the contentScript
+chrome.tabs.query({ url: "*://*.youtube.com/*" }, (tabs) => {
+  for (let tab of tabs) {
+    chrome.tabs.reload(tab.id);
+  }
+});
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   console.log("Service worker received message:", msg);
   
   if (msg.event === "youtube_play") {
-    adjustSpotifyVolume(0.3); // Lower the volume to 30%
+    // Get low volume setting from storage
+    chrome.storage.sync.get(['lowVolume'], function(result) {
+      const lowVolume = result.lowVolume !== undefined ? result.lowVolume / 100 : 0.3;
+      adjustSpotifyVolume(lowVolume);
+    });
   } else if (msg.event === "youtube_pause") {
-    adjustSpotifyVolume(1); // Increase the volume back to 100%
+    // Get high volume setting from storage
+    chrome.storage.sync.get(['highVolume'], function(result) {
+      const highVolume = result.highVolume !== undefined ? result.highVolume / 100 : 1.0;
+      adjustSpotifyVolume(highVolume);
+    });
   }
 });
 
@@ -14,21 +29,18 @@ function adjustSpotifyVolume(volumeLevel) {
   console.log("Adjusting Spotify volume to:", volumeLevel);
   
   chrome.tabs.query({ url: "*://*.spotify.com/*" }, (tabs) => {
-    console.log("Found Spotify tabs:", tabs.length);
-    
     for (let tab of tabs) {
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         func: (volume) => {
+          // This selector is specific to Spotify's web player
+          // If Spotify changes their DOM, this may need to be updated
+          // Or this have to be adjusted to support other platforms
           const volumeInput = document.querySelector('[data-testid="volume-bar"] .hidden-visually input');
-          console.log("volumeInput:", volumeInput);
 
-          console.log("volume =", volume)
           volumeInput.value = volume;
 
-          console.log("new volumeInput.value =", volumeInput.value)
           volumeInput.dispatchEvent(new Event("input", { bubbles: true }));
-          console.log("done")
         },
         args: [volumeLevel]
       }).then((results) => {
